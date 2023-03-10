ff5a143f6b1988107fe9fdc141c2549c23a58827
yohannesTz <yohannes222ethiopia@gmail.com>
2023-03-11 01:21:21 +0300

    initial for threads test

--- app/src/main/java/com/github/yohannestz/koroutinetest/MainActivity.kt
+++ app/src/main/java/com/github/yohannestz/koroutinetest/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.github.yohannestz.koroutinetest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        repeat(1_000_000) {index -> // launch a lot of coroutines
+            Thread {
+                val uiHandler = Handler(Looper.getMainLooper())
+                uiHandler.post {
+                    textView.text = "Update text from $index"
+                }
+            }
+        }
     }
 }
